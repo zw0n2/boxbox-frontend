8eab333e49e322d63367c59fd95b1174cabcdd8e
zw0n2 <daniela0412@naver.com>
2025-12-30 02:21:50 +0900

    supabase-demo

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fmkpzamnlntacumnrxao.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZta3B6YW1ubG50YWN1bW5yeGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMDUwNDAsImV4cCI6MjA4MjU4MTA0MH0.Tr7gzVJyyxooibn33KNxgaciD5GjDwH8C-rjXeIa0nQ';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
